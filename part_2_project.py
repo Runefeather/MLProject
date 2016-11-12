7562c8dd32758cd5bdbc4c91134da8f0e9e1baf2
Runefeather <dhanya7@hotmail.co.uk>
2016-11-12 14:52:06 +0800

    Finished part 2...ish

--- part_2_project.py
+++ part_2_project.py
@@ -1,48 +1,66 @@
-
-# Assume that training data is split over two lists: X and Y 
+# Assume that training data is split over two lists: X and Y
 # where every X[i] contains words in one sentence
 # and Y[i] contains respective tags for that sentence
 # which means, X[i][j] is a word, and Y[i][j] is corresponding tag
 
-def emission_estimates(X, Y, x, y):
-    countYX = 0
+
+# counts number of times y appears in Y
+def countY(Y, tag):
+    total_Y = 0
+    for tags_for_sentence in Y:
+        total_Y += tags_for_sentence.count(tag)
+    return total_Y
+
+# check if x is in X
+def checkX(X, word):
+    for sentence in X:
+        if word in sentence:
+            return True
+
+# get all unique tags in Y
+def getUnique(Y):
+    return list(set(x for l in Y for x in l))
+
+# get emission estimates
+def emissionEstimate(X, Y, x, y):
+    count_YX = 0
     # assume that training set is properly formed
     # which means len(X) == len(Y)
     # and len(X[i]) == len(Y[i]) for all i
-    # I'm looking at you, Arjun. 
+    # I'm looking at you, Arjun.
     length = len(X)
     # if x is in training set
     if checkX(X,x):
         for i in range(0, length):
             for j in range(0, len(X[i])):
                 if Y[i][j] == y and X[i][j] == x:
-                    countYX += 1
-        return (countYX/countY(Y, y))
+                    count_YX += 1
+        return (count_YX/countY(Y, y))
     # if not
     else:
-        return (1/countY(Y, y))
+        return (1/(countY(Y, y) + 1))
 
-# counts number of times y appears in Y
-def countY(Y, y):
-    totalY = 0
-    for yi in Y:
-        totalY += yi.count(y)
-    return totalY
+# Implement a simple sentiment analysis system that produces the tag
+# y∗ = arg max e(x|y)
+# for each word x in the sequence
+# X, Y
+def getTag(X, Y):
+    tags_for_X = list(X)
+    unique_tags = getUnique(Y)
+    for sentence in X:
+        for word in sentence:
+            possible_Y = [0]*len(unique_tags)
+            for i in range(0, len(unique_tags)):
+                possible_Y[i] = emissionEstimate(X, Y, word, unique_tags[i])
+            tags_for_X[X.index(sentence)][sentence.index(word)] = unique_tags[possible_Y.index(max(possible_Y))]
+    return tags_for_X
 
-# check if x is in X
-def checkX(X, x):
-    for xi in X:
-        if x in xi:
-            return True
 
 # test cases
 X = [["the", "cow", "jumped", "over", "the", "moon"], ["the", "dish", "ran", "away", "with", "the", "spoon"]]
 Y = [["D", "N", "V", "P", "D", "N"], ["D", "N", "V", "A", "P", "D", "N"]]
-print("for word in training set:" + str(emission_estimates(X, Y, "the", "D")))
-print("for word in training set:" + str(emission_estimates(X, Y, "the", "P")))
-print("for word not in training set:" + str(emission_estimates(X, Y, "bull", "D")))
-
-
-
-
+print("for word in training set:" + str(emissionEstimate(X, Y, "the", "D")))
+print("for word in training set:" + str(emissionEstimate(X, Y, "the", "P")))
+# this is weird. must email LiHao
+print(getTag(X,Y))
 
